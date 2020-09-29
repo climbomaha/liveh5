33d06dcf8f5fdfe0ce593fc12810f8f6ee05fa83
yuenan <zhaoyuenun@126.com>
2020-09-29 10:42:15 +0800

    fix:测试bridge2

---
+++ build/precache-manifest.3725725e29acb3150252f8c39bdcaf8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa94b70622e56d932032d7d9e9f349b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06101ae9407f9aa3aa2b",
+    "url": "/static/css/main.77581129.chunk.css"
+  },
+  {
+    "revision": "fe9b1a041811b1379c00",
+    "url": "/static/js/2.aeffddc1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aeffddc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06101ae9407f9aa3aa2b",
+    "url": "/static/js/main.bd43aeb4.chunk.js"
+  },
+  {
+    "revision": "e8b417caa6625ee712ae",
+    "url": "/static/js/runtime-main.02716640.js"
+  }
+]);
